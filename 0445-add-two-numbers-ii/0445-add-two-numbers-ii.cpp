4201dd0fcb6016dc8ed4cbbb9b265e865a785cdb
Shruti Sharma <130505209+shruti12sh@users.noreply.github.com>
2023-04-13 13:20:52 +0530

    Time: 35 ms (76.31%), Space: 71.3 MB (39.89%) - LeetHub

--- 0445-add-two-numbers-ii/0445-add-two-numbers-ii.cpp
+++ 0445-add-two-numbers-ii/0445-add-two-numbers-ii.cpp
@@ -1,40 +1,70 @@
 class Solution {
+    private:
+    ListNode* reverse(ListNode* head){
+        ListNode* curr = head;
+        ListNode* prev = NULL;
+        ListNode* next = NULL;
+        while(curr!=NULL){
+            next = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = next;
+        }
+        return prev;
+ }
+ void insert(ListNode* &head, ListNode* &tail,int val){
+     ListNode* temp = new ListNode(val);
+     if(head == NULL){
+         head = temp;
+         tail = temp;
+     }
+     else{
+         tail->next = temp;
+         tail = temp;
+     }
+ }
+ ListNode* add(ListNode* l1, ListNode* l2){
+     int carry = 0;
+     ListNode* anshead= NULL;
+     ListNode* anstail = NULL;
+     while(l1!=NULL && l2!=NULL){
+         int sum = carry+l1->val+l2->val;
+         int digit = sum%10;
+         insert(anshead,anstail,digit);
+         carry = sum/10;
+         l1= l1->next;
+         l2= l2->next;
+     }
+      while(l1!=NULL){
+         int sum = carry+l1->val;
+         int digit = sum%10;
+         insert(anshead,anstail,digit);
+         carry = sum/10;
+         l1= l1->next;
+     }
+      while( l2!=NULL){
+         int sum = carry+l2->val;
+         int digit = sum%10;
+         insert(anshead,anstail,digit);
+         carry = sum/10;
+         l2= l2->next;
+     }
+      while(carry!=0){
+         int sum = carry;
+         int digit = sum%10;
+         insert(anshead,anstail,digit);
+         carry = sum/10;
+        
+     }
+     return anshead;
+ }
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        stack<int> s1, s2;
-        while (l1) {
-            s1.push(l1->val);
-            l1 = l1->next;
-        }
-        while (l2) {
-            s2.push(l2->val);
-            l2 = l2->next;
-        }
-        
-        int carry = 0, first, second;
-        ListNode* head = NULL;
-        while (!s1.empty() || !s2.empty() || carry) {
-            if (!s1.empty()) {
-                first = s1.top();
-                s1.pop();
-            }
-            else
-                first = 0;
-            
-            if (!s2.empty()) {
-                second = s2.top();
-                s2.pop();
-            }
-            else
-                second = 0;
-            
-            int temp = first+second+carry;
-            ListNode* node = new ListNode(temp%10);
-            node->next = head;
-            head = node;
-            carry = temp/10;
-            
-        }
-        return head;
+      l1 = reverse(l1);
+      l2= reverse(l2);
+      //add 
+      ListNode* ans = add(l1, l2);
+       ans = reverse(ans);
+       return ans;
     }
 };
