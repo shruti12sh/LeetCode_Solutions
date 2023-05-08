cdba61c5e3c90394883b29f877f617a101b85784
Shruti Sharma <130505209+shruti12sh@users.noreply.github.com>
2023-05-08 23:19:52 +0530

    Time: 3 ms (56.48%), Space: 10.1 MB (8.13%) - LeetHub

---
+++ 0100-same-tree/0100-same-tree.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    bool isSameTree(TreeNode* p, TreeNode* q) 
+    {
+        if(p==NULL && q==NULL) 
+            return true;
+        if((p==NULL && q!=NULL)||(p!=NULL && q==NULL))
+            return false;
+            
+            
+        bool left=true;  // Left subtree is same or not
+        bool right=true; // Right subtree is same or not
+
+            if(p->val!=q->val)    //if both node have different values
+                return false;
+            else if(p->left==NULL && q->left!=NULL) // p Node don't have left chiled and q node have left chiled Node
+                return false;
+            else if(p->left!=NULL && q->left==NULL)   // p Node have left chiled and q node don't have left chiled Node
+                return false;  
+            else if(p->right==NULL && q->right!=NULL) // p Node don't have right chiled and q node have right chiled Node
+                return false; 
+            else if(p->right!=NULL && q->right==NULL) // p Node have right chiled and q node don't have right chiled Node
+                return false;           
+            
+         
+            if(p->left!=NULL && q->left!=NULL)
+                left=isSameTree(p->left,q->left); //check Recursively left subtree is same or note
+            if(p->right!=NULL && q->right!=NULL)  //check Recursively right subtree is same or note
+                right=isSameTree(p->right,q->right);
+        
+            if(left && right) // if both right side and left side are valid than tree are same
+                return true;
+            else
+                return false;          
+    }
+};
