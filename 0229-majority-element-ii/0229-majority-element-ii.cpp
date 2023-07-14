309ab7d9a80f4c688eaa21056bb7089fa76d474c
Shruti Sharma <130505209+shruti12sh@users.noreply.github.com>
2023-07-14 21:23:01 +0530

    Time: 31 ms (7.18%), Space: 15.8 MB (65.56%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -3,7 +3,7 @@ public:
     vector<int> majorityElement(vector<int>& nums) {
         vector<int>ans;
         for(int i=0;i<nums.size();i++){
-            if(ans.size()==0||ans[0]!=nums[i]){
+            if(ans.size()==0||ans.back()!=nums[i]){
                 int cnt = 0;
                 for(int j=0;j<nums.size();j++){
                     if(nums[j]==nums[i]){
