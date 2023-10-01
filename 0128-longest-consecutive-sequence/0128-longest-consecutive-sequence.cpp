67f3138ded11366883fe18ac6db8c2dd24b4516f
Shruti Sharma <130505209+shruti12sh@users.noreply.github.com>
2023-10-01 10:49:54 +0530

    Time: 136 ms (51.80%), Space: 70.7 MB (9.65%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
@@ -3,7 +3,7 @@ public:
     int longestConsecutive(vector<int>& nums) {
       int n = nums.size();
         if(n==0) return 0;
-        int longest = 1;
+        int longest = 1; 
         unordered_set<int>st;
         for(int i=0;i<n;i++){
             st.insert(nums[i]);
@@ -17,11 +17,14 @@ public:
             while(st.find(x+1)!=st.end()){
                
                 x = x+1;
-                 cnt = cnt+1;
+                cnt = cnt+1;
+
+                
             }
           longest = max(longest,cnt);
             
-        } }
+        } 
+      }
         return longest; 
     }
 };
