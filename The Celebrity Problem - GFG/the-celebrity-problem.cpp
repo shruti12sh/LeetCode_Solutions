12de434523129fe1d8bfa2927e23515417678142
Shruti Sharma <130505209+shruti12sh@users.noreply.github.com>
2023-10-16 17:56:49 +0530

    Added solution - LeetHub

--- The Celebrity Problem - GFG/the-celebrity-problem.cpp
+++ The Celebrity Problem - GFG/the-celebrity-problem.cpp
@@ -9,66 +9,22 @@ using namespace std;
 
 class Solution 
 {
-    
-    
-    private:
-    bool knows(vector<vector<int> >& M,int a ,int b) {
-        if(M[a][b]==1){
-            return true;
-        }
-        else{
-            return false;
-        }
-    }
-    
     public:
     //Function to find if there is a celebrity in the party or not.
     int celebrity(vector<vector<int> >& M, int n) 
     {
         // code here 
-        stack<int>s;
-        for(int i=0;i<n;i++){
-            s.push(i);
-        }
-        while(s.size()>1){
-            int a = s.top();
-            s.pop();
-            int b = s.top();
-            s.pop();
-            if(knows(M,a,b)){
-                s.push(b);
-            }
-            else{
-            s.push(a);
-                
-            }
-            
-        } 
-        int ans = s.top();
-        int zerocount = 0;
-        
-        for(int i =0;i<n;i++){
-            if(M[ans][i]==0)
-                zerocount++;
-            
-        }
-            if(zerocount!= n){
-                return -1;
-                
-            }
-             int onecount = 0;
-        for(int i =0;i<n;i++)
-            if(M[i][ans]==1){
-                onecount++;
-            
-        }
-            if(onecount!= n-1)
-                return -1;
-                
-        
-           return ans; 
-        }
-        
+      int c = 0;
+      for(int i=1;i<n;i++){
+          if(M[c][i]==1){
+              c = i;
+          }
+      }
+      for(int i=0;i<n;i++){
+          if(i!=c && (M[c][i]==1 || M[i][c]==0)) return -1;
+      }
+      return c;
+    }
 };
 
 //{ Driver Code Starts.
