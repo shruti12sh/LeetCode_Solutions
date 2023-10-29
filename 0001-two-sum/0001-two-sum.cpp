4ad3a729ea15cf2884ce55dc09326e889935faf3
Shruti Sharma <130505209+shruti12sh@users.noreply.github.com>
2023-10-29 15:16:25 +0530

    Time: 3 ms (99.24%), Space: 11.2 MB (36.12%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -3,15 +3,16 @@ public:
     vector<int> twoSum(vector<int>& nums, int target) {
          unordered_map<int, int> mpp;
         int n = nums.size();
-    for (int i = 0; i < n; i++) {
-        int num = nums[i];
-        int moreNeeded = target - num;
+        for (int i = 0; i < n; i++) {
+        int moreNeeded = target - nums[i];
         if (mpp.find(moreNeeded) != mpp.end()) {
             return {mpp[moreNeeded], i};
         }
-        mpp[num] = i;
+            
+                 mpp[nums[i]] = i; // Store the current element and its index
+ 
     }
     return { -1, -1};
-}
+   }
     
 };
