5bbf3b34fce9421c6ea26652471a8303c453148c
Shruti Sharma <130505209+shruti12sh@users.noreply.github.com>
2023-10-30 09:59:27 +0530

    Added solution - LeetHub

--- Fractional Knapsack - GFG/fractional-knapsack.cpp
+++ Fractional Knapsack - GFG/fractional-knapsack.cpp
@@ -21,33 +21,36 @@ struct Item{
 class Solution
 {
     public:
+        bool static comp(Item a, Item b) {
+         double r1 = (double) a.value / (double) a.weight;
+         double r2 = (double) b.value / (double) b.weight;
+         return r1 > r2;
+      }
     //Function to get the maximum total value in the knapsack.
-     static bool comp(Item a,Item b){
-        double r1 = (double)a.value/(double)a.weight;
-                double r2 = (double)b.value/(double)b.weight;
-                return r1>r2;
-
-    }
     double fractionalKnapsack(int W, Item arr[], int n)
     {
         // Your code here
-        sort(arr,arr+n,comp);
-        int curr = 0;
-        double val =0.0;
-        for(int i=0;i<n;i++){
-            if(curr+arr[i].weight<=W){
-               curr += arr[i].weight; 
-               val += arr[i].value;
-            }
-            else{
-                int remain = W- curr;
-                val += ((double)arr[i].value/(double)arr[i].weight)*(double)(remain);
-                break;
-            }
-        }
-        return val;
-        
-    }
+    sort(arr, arr + n, comp);
+
+      int curWeight = 0;
+      double finalvalue = 0.0;
+
+      for (int i = 0; i < n; i++) {
+
+         if (curWeight + arr[i].weight <= W) {
+            curWeight += arr[i].weight;
+            finalvalue += arr[i].value;
+         } else {
+            int remain = W - curWeight;
+            finalvalue += (arr[i].value / (double) arr[i].weight) * (double) remain;
+            break;
+         }
+      }
+
+      return finalvalue;
+
+   }
+    
         
 };
 
