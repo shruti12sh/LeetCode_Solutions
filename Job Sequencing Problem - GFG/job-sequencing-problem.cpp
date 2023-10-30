fd1d65e0bae9dbe550e0631740fd64afd9ffb959
Shruti Sharma <130505209+shruti12sh@users.noreply.github.com>
2023-10-30 09:53:23 +0530

    Added solution - LeetHub

--- Job Sequencing Problem - GFG/job-sequencing-problem.cpp
+++ Job Sequencing Problem - GFG/job-sequencing-problem.cpp
@@ -26,40 +26,45 @@ struct Job
 class Solution 
 {
     public:
+    
+    
+     bool static comparison(Job a, Job b) {
+         return (a.profit > b.profit);
+      }
     //Function to find the maximum profit and the number of jobs done.
-     static bool cmp(Job a ,Job b){
-        return a.profit >b.profit;
-    }
     vector<int> JobScheduling(Job arr[], int n) 
     { 
         // your code here
-        vector<int>ans;
-        sort(arr,arr+n,cmp);
-        int maxi = arr[0].dead;
-        for(int i=0;i<n;i++){
-            maxi = max(maxi,arr[i].dead);
-        }
-        int slot[maxi + 1];
-        for(int i=0;i<=maxi;i++)
-            slot[i] = -1;
+    vector<int>ans;
         
-        int countjob = 0, maxprofit = 0;
-        for(int i=0;i<n;i++){
-            
-            for(int j = arr[i].dead;j>0;j--){
-                if(slot[j]==-1){
-                    slot[j]=i;
-                    countjob++;
-                    maxprofit += arr[i].profit;
-                    break;
-                }
+      sort(arr, arr + n, comparison);
+      int maxi = arr[0].dead;
+      for (int i = 1; i < n; i++) {
+         maxi = max(maxi, arr[i].dead);
+      }
+
+      int slot[maxi + 1];
+
+      for (int i = 0; i <= maxi; i++)
+         slot[i] = -1;
+
+      int countJobs = 0, jobProfit = 0;
+
+      for (int i = 0; i < n; i++) {
+         for (int j = arr[i].dead; j > 0; j--) {
+            if (slot[j] == -1) {
+               slot[j] = i;
+               countJobs++;
+               jobProfit += arr[i].profit;
+               break;
             }
-           
-        }
-            ans.push_back(countjob);
-      ans.push_back(maxprofit); 
-     
-      return ans;
+         }
+      }
+      ans.push_back(countJobs);
+      ans.push_back(jobProfit);
+      
+      
+     return ans; 
     } 
 };
 
